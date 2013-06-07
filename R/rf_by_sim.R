0473ab41c0d337bf37c8e5f8c1d7921afb21b094
Karl Broman <kbroman@gmail.com>
2013-06-06 22:54:53 -0500

    Add that figure back

--- R/rf_by_sim.R
+++ R/rf_by_sim.R
@@ -59,6 +59,29 @@ text(0.43, 0.45, "?", col=color[2])
 dev.off()
 
 
+# with the 7r/(1+6r) formula and curve
+pdf("../Figs/rf_by_sim_formula.pdf", width=9, height=6, pointsize=16, onefile=TRUE)
+par(mar=c(4.1, 4.1, 1.1, 1.1),las=1,fg="white",col="white",col.axis="white",col.lab="white",
+    bg=bgcolor,bty="n")
+
+grayplot(rf[wh], onestep[wh], xlab="r", ylab="", xat=xat, yat=yat,
+         hlines=yat, vlines=xat, ylim=c(0, 7/8+0.02), xaxs="i", yaxs="i",
+         pch=21, col="black", bg=color[1], xlim=c(0, 0.5),
+         mgp=c(2.1, 0.4, 0))
+mtext(side=2, "R", srt=90, line=2.6)
+abline(h=7/8, col=color2[3], lwd=2)
+u <- par("usr")
+mtext(side=2, at=7/8, "7/8", col=color2[3], line=0.4)
+
+x <- seq(0, 0.5, length=251)
+lines(x, 7*x/(1+6*x), col=color2[1], lwd=2)
+points(rf[wh], onestep[wh], col="black", bg=color[1], pch=21)
+
+rect(0.305, 0.403, 0.445, 0.497, col="gray80", border="gray80", alpha=0.7)
+text(0.32, 0.45, "R = 7 r / (1 + 6 r)", col="black", adj=c(0, 0.5))
+dev.off()
+
+
 # with additional data points
 pdf("../Figs/rf_by_sim_moredata.pdf", width=9, height=6, pointsize=16, onefile=TRUE)
 par(mar=c(4.1, 4.1, 1.1, 1.1),las=1,fg="white",col="white",col.axis="white",col.lab="white",
