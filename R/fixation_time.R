8ddae35903247e550b19ff9118ec99002edd9a76
Karl Broman <kbroman@gmail.com>
2013-06-09 00:05:23 -0500

    fixation time figure: use grayplot

--- R/fixation_time.R
+++ R/fixation_time.R
@@ -20,22 +20,23 @@ function(k, nstr=c("2","4")) {
 
 }
 
+library(broman)
 source("colors.R")
-color <- c(rgb(102,203,254,maxColorValue=255),
-           rgb(254,102,254,maxColorValue=255),
-           rgb(102,254,102,maxColorValue=255),
-           rgb(254,203,102,maxColorValue=255)) # yellow
+color <- c(sexcolor[2], hetcolor, sexcolor[1])
 
 pdf("../Figs/fixation_time.pdf", height=6, width=9, pointsize=16, onefile=TRUE)
 maxgen <- 20
-par(mar=c(4.1,4.1,1.1,1.1),las=1,fg="white",col="white",
+par(mar=c(3.1,3.6,1.1,1.1),las=1,fg="white",col="black",
     col.axis="white",col.lab=textcolor["blue"],
     bg=bgcolor)
-plot(0:maxgen, c(0,self(1:maxgen)), xlab=expression(paste("Generation ", F[k])),
-     ylab=expression(paste("Pr( fixed  by  ", F[k], " )")),
-     type="l", lwd=2, col=color[1],
-     xaxs="i", yaxs="i", ylim=c(0, 1))
-axis(side=1, at=c(1:4, 6:9, 11:14, 16:19, 21:24), labels=rep("",20), tck=-0.015)
+grayplot(0:maxgen, c(0,self(1:maxgen)), xlab=expression(paste("Generation ", F[k])),
+         ylab=expression(paste("Pr( fixed  by  ", F[k], " )")),
+         type="l", lwd=2, col=color[1],
+         xaxs="i", yaxs="i", ylim=c(0, 1),
+         xat=seq(0, 20, by=5), yat=seq(0, 1, by=0.2),
+         vlines=1:20, hlines=seq(0, 1, by=0.1),
+         vlines.col=c(rep("gray90", 4), "gray60"), hlines.col="gray90",
+         mgp.x=c(1.7, 0.3, 0), mgp.y=c(2.1, 0.4, 0))
 lines(0:maxgen, sibA(0:maxgen, "2"), lwd=2, col=color[2])
 lines(0:maxgen, sibX(0:maxgen, "2"), lwd=2, col=color[2], lty=2)
 lines(0:maxgen, sibA(0:maxgen, "4"), lwd=2, col=color[3])
@@ -43,7 +44,6 @@ lines(0:maxgen, sibX(0:maxgen, "4"), lwd=2, col=color[3], lty=2)
 legend("bottomright",
        c("selfing", "2-way, sib-mating, X chr", "2-way, sib-mating, autosome", 
          "4-way, sib-mating, X chr", "4-way, sib-mating, autosome"), lwd=2,
-       col=color[c(1,2,2,3,3)], lty=c(1,2,1,2,1))
-u <- par("usr")
-text(u[1]-diff(u[1:2])*0.1, u[4]+diff(u[3:4])*0.16, "A", font=2, xpd=TRUE, cex=1.3)
+       col=color[c(1,2,2,3,3)], lty=c(1,2,1,2,1),
+       bg="gray80", box.col="black", box.lwd=2)
 dev.off()
